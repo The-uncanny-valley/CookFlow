331dfc44046370479b2f056b37a80bdf2bc71fee
the-uncanny-valley <runcannyvalley@gmail.com>
2025-09-26 16:43:02 +0300

    feat(network): add RecipeApi interface and RecipeApiImpl implementation

---
+++ app/src/main/java/com/uncannyvalley/cookflow/data/remote/api/RecipeApi.kt
@@ -0,0 +1,23 @@
+package com.uncannyvalley.cookflow.data.remote.api
+
+import com.uncannyvalley.cookflow.BuildConfig
+import com.uncannyvalley.cookflow.data.remote.dto.RecipeDto
+import com.uncannyvalley.cookflow.data.remote.dto.RecipeResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface RecipeApi {
+    @GET("recipes/complexSearch")
+    suspend fun getRecipes(
+        @Query("query") query: String? = null,
+        @Query("type") type: String? = null,
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY
+    ): RecipeResponse
+
+    @GET("recipes/{id}/information")
+    suspend fun getRecipeById(
+        @Path("id") id: Int,
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY
+    ): RecipeDto
+}
---
+++ app/src/main/java/com/uncannyvalley/cookflow/data/remote/api/RecipeApiImpl.kt
@@ -0,0 +1,19 @@
+package com.uncannyvalley.cookflow.data.remote.api
+
+import com.uncannyvalley.cookflow.data.remote.dto.RecipeDto
+import com.uncannyvalley.cookflow.data.remote.dto.RecipeResponse
+import javax.inject.Inject
+
+class RecipeApiImpl @Inject constructor(
+    private val api: RecipeApi,
+    private val apiKey: String
+) {
+
+    suspend fun getRecipes(query: String? = null, type: String? = null): RecipeResponse {
+        return api.getRecipes(query, type, apiKey)
+    }
+
+    suspend fun getRecipeById(id: Int): RecipeDto {
+        return api.getRecipeById(id, apiKey)
+    }
+}
