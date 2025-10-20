46e90ecea48801daa1b34bf938d7acd3c5ca611c
the-uncanny-valley <runcannyvalley@gmail.com>
2025-10-20 18:36:44 +0300

    chore(theme): remove unused color definitions from Color.kt

--- app/src/main/java/com/uncannyvalley/cookflow/presentation/theme/Color.kt
+++ app/src/main/java/com/uncannyvalley/cookflow/presentation/theme/Color.kt
@@ -7,12 +7,6 @@ val neutral30 = Color(0xFFC1C1C1)
 val neutral40 = Color(0xFFA9A9A9)
 val neutral90 = Color(0xFF303030)
 
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val primary = Color(0xFFE23E3E)
 val secondary = Color(0xFFEE8B8B)
 
